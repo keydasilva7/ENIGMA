94b786d34d1e133afa0218ef52a358f38a477dbb
Agustina Crego <116461112+Agusrc20@users.noreply.github.com>
2026-05-10 22:51:30 -0300

    Add files via upload
    - Se establecen las medidas del tablero con macros: 20 filas visibles, 4 ocultas y 10 columnas.
    - Se implementa la estructura 'Tetromino' que contiene el tipo de pieza, la posición x e y, su matriz forma (4x4) que guarda la pieza.
    - Se crea la estructura 'EstadoJuego' que contiene el tablero, puntos, líneas, velocidad, pieza actual, siguiente, caida y game over.
    - Se declaran los prototipos esenciales para las funciones de inicialización, rotación y generación de piezas.

--- tetris.h
+++ tetris.h
@@ -12,18 +12,47 @@
 #define C 3  // Celeste
 #define V 2  // Verde
 
+#define FILAS_VISIBLES 20
+#define FILAS_OCULTAS 4
+#define FILAS_TOTALES 24
+#define COLUMNAS 10
+
 
 //Nuestra estructura de pieza (I, J, L, O, S, T, Z)
 //Permite mover copiar o colisionar
 typedef struct
 {
-    int tipo;      // 0 a 6 (qué pieza es)
-    int x, y;      // posición en el tablero
-    int rotacion;  // 0 a 3
+    int tipo;      // qué pieza es? I=0, J=1, L=2, O=3, S=4, T=5, Z=6
+    int x, y;      // es la posición en el tablero, sería como coordenadas en un eje x e y pero en realidad sería x la columna e y la fila
+    int forma[4][4];  // es la forma que va a tomar
 
 } Tetromino;
 
-void dibujar(const uint8_t[][PIXELES_X_LADO], uint16_t oX, uint16_t oY);
-void rotar(int [][4], int [][4]);
+// Estado del juego
+typedef struct
+{
+    int tablero[FILAS_TOTALES][COLUMNAS]; // 0 si esta vacio, color si esta ocupado
+
+    Tetromino pieza_actual;
+    Tetromino pieza_siguiente;
+
+    long puntos;
+    int lineas;
+    int piezas_caidas;
+
+    float velocidad_caida_ms; // milisegundos por cada caida de fila
+    int game_over;
+
+} EstadoJuego;
+
+void rotar_pieza(int origen[4][4], int destino[4][4]);
+void dibujar(const uint8_t dibujo[][PIXELES_X_LADO], uint16_t oX, uint16_t oY);
+void inicializar_tablero(EstadoJuego* estado);
+void inicializar_estructura(EstadoJuego* estado);
+void inicializar_juego(EstadoJuego* estado);
+void generar_nueva_pieza(EstadoJuego* estado);
+
+
+
 
 #endif // TETRIS_H_INCLUDED
