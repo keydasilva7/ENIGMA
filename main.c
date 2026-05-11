f701c79f40176cea6c45a111e281a0a3436c99da
Agustina Crego <116461112+Agusrc20@users.noreply.github.com>
2026-05-10 22:55:24 -0300

    Add files via upload
    - Modifico los comentarios al comienzo del archivo con información sobre los integrantes

--- main.c
+++ main.c
@@ -1,22 +1,21 @@
 /*
-Apellido(s), nombre(s): Marcos Kouvach
+Apellido(s), nombre(s): Kouvach, Marcos
 DNI: 45013925
 Usuario: mkouvach
 Entrega: Si
 
-Apellido(s), nombre(s): Marcos Kouvach
-DNI: 45013925
-Usuario: mkouvach
+Apellido(s), nombre(s): Alvarez da Silva, Keila
+DNI: 45236512
+Usuario: keydasilva7
 Entrega: Si
 
 
-Apellido(s), nombre(s): Marcos Kouvach
-DNI: 45013925
-Usuario: mkouvach
+Apellido(s), nombre(s): Crego, Agustina Rocio
+DNI: 44689109
+Usuario: Agusrc20
 Entrega: Si
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "GBT/gbt.h"
@@ -30,9 +29,6 @@ Entrega: Si
 #define CANT_COLORES 16
 #define TAM_GRILLA 11
 
-#define FILAS_VISIBLES 20
-#define FILAS_OCULTAS 4
-#define COLUMNAS 10
 
 
 tGBT_ColorRGB paletaCGA[CANT_COLORES] =
@@ -56,15 +52,33 @@ tGBT_ColorRGB paletaCGA[CANT_COLORES] =
     {0xFF, 0xFF, 0x55}, // 14:  Amarillo
     {0xFF, 0xFF, 0xFF}  // 15:  Usado como transparente por GBT
 };
-
 int main(int argc, char* argv[])
 {
-    
-
-
-
 
 
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
