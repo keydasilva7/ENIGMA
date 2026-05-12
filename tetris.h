7f9e235d654a28befe021898347e7719e0b8978d
keydasilva7 <keialvarezdasilva@hotmail.com>
2026-05-12 10:42:17 -0300

    Prototipos de funciones nuevas
    Añadimos el prototipo de las funciones desarrolladas y agregamos algunos comentarios

--- tetris.h
+++ tetris.h
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include "GBT/gbt.h"
 
-#define PIXELES_X_LADO 8
-#define PX_PADDING 4
+#define PIXELES_X_LADO 8 //medida de un bloque
+#define PX_PADDING 4 //separacion entre bloques
 
 #define N 15 // Transparente
 #define A 14 // Amarillo
@@ -22,8 +22,8 @@
 //Permite mover copiar o colisionar
 typedef struct
 {
-    int tipo;      // qué pieza es? I=0, J=1, L=2, O=3, S=4, T=5, Z=6
-    int x, y;      // es la posición en el tablero, sería como coordenadas en un eje x e y pero en realidad sería x la columna e y la fila
+    int tipo;      // quÃ© pieza es? I=0, J=1, L=2, O=3, S=4, T=5, Z=6
+    int x, y;      // es la posiciÃ³n en el tablero, serÃ­a como coordenadas en un eje x e y pero en realidad serÃ­a x la columna e y la fila
     int forma[4][4];  // es la forma que va a tomar
 
 } Tetromino;
@@ -50,6 +50,10 @@ void dibujar(const uint8_t dibujo[][PIXELES_X_LADO], uint16_t oX, uint16_t oY);
 void inicializar_tablero(EstadoJuego* estado);
 void inicializar_estructura(EstadoJuego* estado);
 void inicializar_juego(EstadoJuego* estado);
+int puede_mover_pieza(EstadoJuego* estado, int dx, int dy);
+void mover_pieza(EstadoJuego* estado, int dx, int dy);
+void dibujar_pieza(EstadoJuego* estado);
+void fijar_pieza(EstadoJuego* estado);
 void generar_nueva_pieza(EstadoJuego* estado);
 
 
