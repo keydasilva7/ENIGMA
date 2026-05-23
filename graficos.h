eee0e8d81e510a3a673fc079c22e501355bf88ad
keydasilva7 <keialvarezdasilva@hotmail.com>
2026-05-23 04:10:06 -0300

    Modificaciones varias Generales
    Modificaciones varias Generales
    -ESTADO_PAUSADO y ESTADO_INGRESO_NOMBRE
    Agregamos la funcionalidad para pausar el juego y la solicitud de ingresar nombre al usuario para guardarlo en un archivo junto con la mejor puntuacion, faltan fixes con la bibloteca GBT pero hay avances.
    -Agregamos la dificultad del juego (velocidad de caida, de fijacion y relacion con puntaje)
    -Orden de funciones y comentarios agregados generalmente en todo el proyecto
    -eliminacion de copiar pieza (funcion inutilizada)
    -dibujar_rectangulo y actualizar_ingreso_nombre (nuevas funciones graficos.c para menu e interfaz de jugador)

--- graficos.h
+++ graficos.h
@@ -16,15 +16,27 @@ typedef struct
 
 }ResolucionVentana;
 
-int inicializar_graficos(ResolucionVentana*,int,int);
-void dibujar_bloque_cuadrado(ResolucionVentana*, int,int,int);
-void dibujar_pieza(EstadoJuego *, ResolucionVentana* );
-void dibujar_tablero(EstadoJuego*, ResolucionVentana* );
+// Inicializacion
+int inicializar_graficos(ResolucionVentana*, int, int);
+
+
+void dibujar_bloque_cuadrado(ResolucionVentana*, int, int, int);
+void dibujar_rectangulo(int, int, int, int, int);
 void dibujar_caracter(char, uint16_t, uint16_t, uint8_t);
 void dibujar_texto(const char*, uint16_t, uint16_t, uint8_t);
+
+// Pantallas
+void dibujar_tablero(EstadoJuego*, ResolucionVentana* );
+void dibujar_pieza(EstadoJuego *, ResolucionVentana* );
 void dibujar_ui(EstadoJuego *, ResolucionVentana *);
-void dibujar_interfaz_game_over(EstadoJuego*, ResolucionVentana*);
+void dibujar_menu(int);
+void dibujar_pantalla_pausa(ResolucionVentana*);
+void dibujar_interfaz_game_over(EstadoJuego*, ResolucionVentana*, Jugador*);
 
+//Var Global de Ventana (utilizada para funciones que no reciben ventana)
+extern ResolucionVentana* g_ventana;
 
+//Jugador
+int actualizar_ingreso_nombre(eGBT_Tecla, Jugador*, ResolucionVentana*);
 
 #endif // GRAFICOS_H_INCLUDED
