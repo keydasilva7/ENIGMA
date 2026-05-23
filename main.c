93f7ac7dc345289c11300766d46a3a8165d36115
keydasilva7 <keialvarezdasilva@hotmail.com>
2026-05-23 04:05:59 -0300

    Modificaciones varias Generales
    -ESTADO_PAUSADO y ESTADO_INGRESO_NOMBRE
    Agregamos la funcionalidad para pausar el juego y la solicitud de ingresar nombre al usuario para guardarlo en un archivo junto con la mejor puntuacion, faltan fixes con la bibloteca GBT pero hay avances.
    -Agregamos la dificultad del juego (velocidad de caida, de fijacion y relacion con puntaje)
    -Orden de funciones y comentarios agregados generalmente en todo el proyecto
    -eliminacion de copiar pieza (funcion inutilizada)
    -dibujar_rectangulo y actualizar_ingreso_nombre (nuevas funciones graficos.c para menu e interfaz de jugador)

--- main.c
+++ main.c
@@ -27,33 +27,50 @@ Entrega: Si
 
 #define CANT_COLORES 16
 
-typedef enum {
+typedef enum
+{
     ESTADO_MENU,
+    ESTADO_INGRESO_NOMBRE,
     ESTADO_AJUSTES,
     ESTADO_JUGANDO,
+    ESTADO_PAUSADO,
     ESTADO_GAMEOVER
 } EstadoAplicacion;
 
 // Prototipos modificados (ahora actualizar_ajustes recibe un puntero a entero)
-void actualizar_menu(EstadoAplicacion* estado_app, eGBT_Tecla tecla, int* corriendo, EstadoJuego* estado_juego, tGBT_Temporizador** timer_caida);
-void actualizar_ajustes(EstadoAplicacion* estado_app, eGBT_Tecla tecla, ResolucionVentana* ventana, int* sub_menu);
-void actualizar_jugando(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana, tGBT_Temporizador* timer_caida);
-void actualizar_gameover(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana);
+void actualizar_menu(EstadoAplicacion* estado_app, eGBT_Tecla tecla, int* corriendo, EstadoJuego* estado_juego, tGBT_Temporizador** timer_caida, tGBT_Temporizador** timer_fijacion, int* opcion_menu, Jugador* jug);
+void actualizar_ajustes(EstadoAplicacion* estado_app, eGBT_Tecla tecla, ResolucionVentana* ventana, int* sub_menu, Jugador* jug);
+void actualizar_jugando(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana, tGBT_Temporizador** timer_caida, tGBT_Temporizador** timer_fijacion);
+void actualizar_pausado(EstadoAplicacion* estado_app, eGBT_Tecla tecla, ResolucionVentana* ventana);
+void actualizar_gameover(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana, Jugador* jug);
 int reiniciar_entorno_grafico(ResolucionVentana* ventana);
+void actualizar_velocidad(EstadoJuego* estado_juego, tGBT_Temporizador** timer_caida, tGBT_Temporizador** timer_fijacion);
 
-tGBT_ColorRGB paletaCGA[CANT_COLORES] = {
+tGBT_ColorRGB paletaCGA[CANT_COLORES] =
+{
     {0x00, 0x00, 0x00}, {0x00, 0x00, 0xAA}, {0x00, 0xAA, 0x00}, {0x00, 0xAA, 0xAA},
     {0xAA, 0x00, 0x00}, {0xAA, 0x00, 0xAA}, {0xAA, 0x55, 0x00}, {0xAA, 0xAA, 0xAA},
     {0x55, 0x55, 0x55}, {0x55, 0x55, 0xFF}, {0x55, 0xFF, 0x55}, {0x55, 0xFF, 0xFF},
     {0xFF, 0x55, 0x55}, {0xFF, 0x55, 0xFF}, {0xFF, 0xFF, 0x55}, {0xFF, 0xFF, 0xFF}
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     ResolucionVentana ventana;
     ventana.es_vga = 0;
     ventana.escala = 2;
+    Jugador jug;
 
-    if (inicializar_graficos(&ventana, ventana.escala, ventana.es_vga) == -1) {
+    memset(&jug, 0, sizeof(jug));
+    if(cargar_jugador(&jug))
+    {
+        ventana.es_vga = jug.es_vga;
+        ventana.escala = jug.escala;
+        jug.nombre[0] = '\0';
+    }
+
+    if (inicializar_graficos(&ventana, ventana.escala, ventana.es_vga) == -1)
+    {
         fprintf(stderr, "Error al iniciar entorno grafico base.\n");
         return -1;
     }
@@ -63,29 +80,48 @@ int main(int argc, char* argv[]) {
     EstadoAplicacion estado_app = ESTADO_MENU;
     EstadoJuego estado_juego;
     tGBT_Temporizador* timer_caida = NULL;
+    tGBT_Temporizador* timer_fijacion = NULL;
     int corriendo = 1;
-
     int sub_menu_ajustes = 0;
+    int opcion_menu = 0;
 
-    while (corriendo) {
+    while (corriendo)
+    {
         gbt_procesar_entrada();
         eGBT_Tecla tecla = gbt_obtener_tecla_presionada();
         gbt_borrar_backbuffer(0);
 
-        switch (estado_app) {
-            case ESTADO_MENU:
-                actualizar_menu(&estado_app, tecla, &corriendo, &estado_juego, &timer_caida);
-                break;
-            case ESTADO_AJUSTES:
-                // Pasamos la variable por referencia para que la funci�n pueda leerla y modificarla
-                actualizar_ajustes(&estado_app, tecla, &ventana, &sub_menu_ajustes);
-                break;
-            case ESTADO_JUGANDO:
-                actualizar_jugando(&estado_app, tecla, &estado_juego, &ventana, timer_caida);
-                break;
-            case ESTADO_GAMEOVER:
-                actualizar_gameover(&estado_app, tecla, &estado_juego, &ventana);
-                break;
+        switch (estado_app)
+        {
+        case ESTADO_MENU:
+            actualizar_menu(&estado_app, tecla, &corriendo, &estado_juego, &timer_caida, &timer_fijacion, &opcion_menu, &jug);
+            break;
+        case ESTADO_AJUSTES:
+            // Pasamos la variable por referencia para que la funcion pueda leerla y modificarla
+            actualizar_ajustes(&estado_app, tecla, &ventana, &sub_menu_ajustes, &jug);
+            break;
+        case ESTADO_JUGANDO:
+            actualizar_jugando(&estado_app, tecla, &estado_juego, &ventana, &timer_caida, &timer_fijacion);
+            break;
+        case ESTADO_PAUSADO:
+            actualizar_pausado(&estado_app, tecla, &ventana);
+            break;
+        case ESTADO_GAMEOVER:
+            actualizar_gameover(&estado_app, tecla, &estado_juego, &ventana, &jug);
+            break;
+        case ESTADO_INGRESO_NOMBRE:
+            if (actualizar_ingreso_nombre(tecla, &jug, &ventana))
+            {
+                // Jugador confirmo nombre -> inicia juego
+                strncpy(estado_juego.nombre_jugador, jug.nombre, 31);
+                inicializar_juego(&estado_juego);
+                if (timer_caida) gbt_temporizador_destruir(timer_caida);
+                timer_fijacion = NULL;
+                double seg = estado_juego.velocidad_caida_ms / 1000.0;
+                timer_caida = gbt_temporizador_crear(seg);
+                estado_app = ESTADO_JUGANDO;
+            }
+            break;
         }
 
         gbt_volcar_backbuffer();
@@ -93,6 +129,7 @@ int main(int argc, char* argv[]) {
     }
 
     if (timer_caida) gbt_temporizador_destruir(timer_caida);
+    if (timer_fijacion) gbt_temporizador_destruir(timer_fijacion);
     gbt_destruir_ventana();
     gbt_cerrar();
 
@@ -100,11 +137,82 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-void actualizar_ajustes(EstadoAplicacion* estado_app, eGBT_Tecla tecla, ResolucionVentana* ventana, int* sub_menu) {
+//-----Velocidad: recalcula ambos timers según piezas_caidas
+//cada 10 piezas la velocidad sube un 3% (multiplicamos por 0.97)
+
+void actualizar_velocidad(EstadoJuego* ej, tGBT_Temporizador** tc, tGBT_Temporizador** tf)
+{
+    //actualiza la velocidad_caida_ms cada vez que cae una pieza
+    if(*tc)
+    {
+        gbt_temporizador_destruir(*tc);
+    }
+    else
+    {
+        *tc = NULL;
+    }
+    if(*tf)
+    {
+        gbt_temporizador_destruir(*tf);
+    }
+    else
+    {
+        *tf = NULL;
+    }
+
+    double seg_caida = ej->velocidad_caida_ms / 1000.0;
+    *tc = gbt_temporizador_crear(seg_caida);
+}
+//--MENU - selector visual con flechas
+
+void actualizar_menu(EstadoAplicacion* estado_app, eGBT_Tecla tecla, int* corriendo,EstadoJuego* estado_juego, tGBT_Temporizador** timer_caida, tGBT_Temporizador** timer_fijacion, int* opcion_menu, Jugador* jug)
+{
+    // Mover selector con flechas
+    if (tecla == GBTK_ARRIBA) *opcion_menu = (*opcion_menu + 2) % 3;
+    if (tecla == GBTK_ABAJO)  *opcion_menu = (*opcion_menu + 1) % 3;
+
+    // Atajos numericos
+    if (tecla == GBTK_1) *opcion_menu = 0;
+    if (tecla == GBTK_2) *opcion_menu = 1;
+    if (tecla == GBTK_3) *opcion_menu = 2;
+
+    dibujar_menu(*opcion_menu);
+
+    // Confirmar
+    int confirmar = (tecla == GBTK_ENTER || tecla == GBTK_ESPACIO ||
+                     tecla == GBTK_1 || tecla == GBTK_2 || tecla == GBTK_3);
+
+    if (confirmar)
+    {
+        if (*opcion_menu == 0)
+        {
+            // JUGAR -> pedir nombre primero
+            jug->nombre[0]='\0';
+            *estado_app = ESTADO_INGRESO_NOMBRE;
+        }
+        else if (*opcion_menu == 1)
+        {
+            // AJUSTES
+            *estado_app = ESTADO_AJUSTES;
+        }
+        else
+        {
+            // SALIR
+            *corriendo = 0;
+        }
+    }
+
+    if (tecla == GBTK_ESCAPE) *corriendo = 0;
+}
+
+//--Ajustes
+
+void actualizar_ajustes(EstadoAplicacion* estado_app, eGBT_Tecla tecla, ResolucionVentana* ventana, int* sub_menu, Jugador* jug)
+{
     char buffer_texto[64];
 
-    // Ahora usamos el operador de desreferencia (*) para acceder al valor original
-    if (*sub_menu == 0) {
+    if (*sub_menu == 0)
+    {
         dibujar_texto("--- AJUSTES ---", 40, 20, 11);
         sprintf(buffer_texto, "Modo actual: %s", ventana->es_vga ? "VGA" : "CGA");
         dibujar_texto(buffer_texto, 20, 50, 7);
@@ -116,93 +224,156 @@ void actualizar_ajustes(EstadoAplicacion* estado_app, eGBT_Tecla tecla, Resoluci
 
         if (tecla == GBTK_1) *sub_menu = 1;
         else if (tecla == GBTK_2) *sub_menu = 2;
-        else if (tecla == GBTK_3 || tecla == GBTK_ESCAPE) {
+        else if (tecla == GBTK_3 || tecla == GBTK_ESCAPE)
             *estado_app = ESTADO_MENU;
-        }
     }
-    else if (*sub_menu == 1) {
+    else if (*sub_menu == 1)
+    {
         dibujar_texto("--- MODO GRAFICO ---", 40, 20, 11);
         dibujar_texto("0. CGA (320x200)", 20, 70, 14);
         dibujar_texto("1. VGA (640x480)", 20, 90, 14);
         dibujar_texto("Presione 0 o 1 (ESC atras)", 20, 140, 8);
 
-        if (tecla == GBTK_0) {
+        if (tecla == GBTK_0)
+        {
             ventana->es_vga = 0;
             reiniciar_entorno_grafico(ventana);
-            *sub_menu = 0;
-        } else if (tecla == GBTK_1) {
-            ventana->es_vga = 1;
-            reiniciar_entorno_grafico(ventana);
-            *sub_menu = 0;
-        } else if (tecla == GBTK_ESCAPE) {
+            jug->es_vga = 0;
+            guardar_jugador(jug);
             *sub_menu = 0;
         }
+        else if (tecla == GBTK_1)
+        {
+            ventana->es_vga = 1;
+            reiniciar_entorno_grafico(ventana);
+            jug->es_vga = 1;
+            guardar_jugador(jug);
+            *sub_menu = 0;
+        }
+        else if (tecla == GBTK_ESCAPE)
+            *sub_menu = 0;
     }
-    else if (*sub_menu == 2) {
+    else if (*sub_menu == 2)
+    {
         dibujar_texto("--- CAMBIAR ESCALA ---", 40, 20, 11);
         dibujar_texto("1. Escala 1", 20, 60, 14);
         dibujar_texto("2. Escala 2", 20, 80, 14);
         dibujar_texto("3. Escala 3", 20, 100, 14);
-        dibujar_texto("Presione 1, 2 o 3 (ESC atras)", 20, 140, 8);
+        dibujar_texto("4. Escala 4", 20, 120, 14);
+        dibujar_texto("Presione 1, 2, 3 o 4 (ESC atras)", 20, 140, 8);
 
-        if (tecla == GBTK_1) { ventana->escala = 1; reiniciar_entorno_grafico(ventana); *sub_menu = 0; }
-        else if (tecla == GBTK_2) { ventana->escala = 2; reiniciar_entorno_grafico(ventana); *sub_menu = 0; }
-        else if (tecla == GBTK_3) { ventana->escala = 3; reiniciar_entorno_grafico(ventana); *sub_menu = 0; }
-        else if (tecla == GBTK_ESCAPE) *sub_menu = 0;
+        int nueva_escala = 0;
+        if      (tecla == GBTK_1) nueva_escala = 1;
+        else if (tecla == GBTK_2) nueva_escala = 2;
+        else if (tecla == GBTK_3) nueva_escala = 3;
+        else if (tecla == GBTK_4) nueva_escala = 4;
+        else if (tecla == GBTK_ESCAPE) { *sub_menu = 0; return; }
+
+        if (nueva_escala > 0)
+        {
+            ventana->escala = nueva_escala;
+            reiniciar_entorno_grafico(ventana);
+            jug->escala = nueva_escala;
+            guardar_jugador(jug);
+            *sub_menu = 0;
+        }
     }
 }
 
-void actualizar_menu(EstadoAplicacion* estado_app, eGBT_Tecla tecla, int* corriendo, EstadoJuego* estado_juego, tGBT_Temporizador** timer_caida) {
-    dibujar_texto("=== TETRIS ===", 40, 20, 14);
-    dibujar_texto("1. JUGAR", 40, 60, 10);
-    dibujar_texto("2. AJUSTES", 40, 80, 11);
-    dibujar_texto("3. SALIR", 40, 100, 12);
-    dibujar_texto("Presione el numero [1-3]", 30, 150, 7);
-
-    if (tecla == GBTK_1) {
-        inicializar_juego(estado_juego);
-        if (*timer_caida) gbt_temporizador_destruir(*timer_caida);
-        *timer_caida = gbt_temporizador_crear(1.0);
-        *estado_app = ESTADO_JUGANDO;
-    } else if (tecla == GBTK_2) {
-        *estado_app = ESTADO_AJUSTES;
-    } else if (tecla == GBTK_3 || tecla == GBTK_ESCAPE) {
-        *corriendo = 0;
-    }
-}
-
-void actualizar_jugando(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana, tGBT_Temporizador* timer_caida) {
-    if (gbt_tecla_presionada(GBTK_ESCAPE)) {
+//--Jugando
+void actualizar_jugando(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana, tGBT_Temporizador** timer_caida, tGBT_Temporizador** timer_fijacion)
+{
+    //Pausa
+    if (tecla == GBTK_ESCAPE)
+    {
         *estado_app = ESTADO_MENU;
         return;
     }
+    if (tecla == GBTK_p)
+    {
+        *estado_app = ESTADO_PAUSADO;  // P → pausa
+        return;
+    }
 
+    //--Movimiento horizontal
     if (tecla == GBTK_IZQUIERDA)       mover_pieza(estado_juego, -1, 0);
     else if (tecla == GBTK_DERECHA)    mover_pieza(estado_juego, 1, 0);
-    else if (tecla == GBTK_ABAJO)
-    {
-        mover_pieza(estado_juego, 0, 1);
-        estado_juego->puntos++;
-    }
-    else if (tecla == GBTK_ARRIBA || tecla == GBTK_ESPACIO) rotar_pieza_actual(estado_juego, 1);
 
-    if (gbt_temporizador_consumir(timer_caida)) {
-        if (puede_mover_pieza(estado_juego, 0, 1)) {
+    //--Rotacion izquierda y derecha
+    else if (tecla == GBTK_ARRIBA || tecla == GBTK_ESPACIO) rotar_pieza_actual(estado_juego, 1);
+    else if (tecla == GBTK_z) rotar_pieza_actual(estado_juego, -1);
+
+    //--Bajar Rapido
+    else if (tecla == GBTK_ABAJO )
+    {
+        if(puede_mover_pieza(estado_juego, 0, 1))
+        {
             mover_pieza(estado_juego, 0, 1);
-        } else {
+
+            //A mayor velocidad, la pieza cae mas rapido sola, mayor puntos por jugador
+            estado_juego->puntos += (long)(1000.0f / estado_juego -> velocidad_caida_ms);
+        }
+    }
+
+    if (gbt_temporizador_consumir(*timer_caida))
+    {
+        if (puede_mover_pieza(estado_juego, 0, 1))
+        {
+            mover_pieza(estado_juego, 0, 1);
+            // La pieza bajo -> destruimos timer de fijación si es que existia
+            {
+                gbt_temporizador_destruir(*timer_fijacion);
+                *timer_fijacion = NULL;
+            }
+        }
+        else
+        {
+            // La pieza NO puede bajar -> activamos timer de fijacion si no esta corriendo
+            if (*timer_fijacion == NULL)
+            {
+                double seg_fij = (estado_juego->velocidad_caida_ms * 0.5) / 1000.0;
+                *timer_fijacion = gbt_temporizador_crear(seg_fij);
+            }
+        }
+    }
+    //--Timer de fijacion
+    //Solo fija si la pieza NO puede seguir bajando porque toca la base o choca con otra pieza
+    if(*timer_fijacion && gbt_temporizador_consumir(*timer_fijacion))
+    {
+        gbt_temporizador_destruir(*timer_fijacion);
+        *timer_fijacion = NULL;
+
+        if(!puede_mover_pieza(estado_juego, 0, 1))
+        {
+            int piezas_antes = estado_juego->piezas_caidas;
+
             fijar_pieza(estado_juego);
             borrar_lineas_completas(estado_juego);
             estado_juego->pieza_actual = estado_juego->pieza_siguiente;
             generar_nueva_pieza(estado_juego);
 
-            if (!puede_mover_pieza(estado_juego, 0, 0)) {
+            //bonus de puntos por velocidad: mas velocidad mas puntos al fijar
+            long bonus_velocidad = (long)(1000.0f / estado_juego->velocidad_caida_ms * 10);
+            estado_juego->puntos += bonus_velocidad;
+
+            //Cada 10 piezas subimos dificultad
+            if((estado_juego->piezas_caidas / 10) > (piezas_antes / 10))
+            {
+                recalcular_velocidad(estado_juego);
+                actualizar_velocidad(estado_juego, timer_caida, timer_fijacion);
+            }
+
+            if(!puede_mover_pieza(estado_juego, 0, 0))
+            {
                 estado_juego->game_over = 1;
             }
         }
     }
 
-    if (estado_juego->game_over) {
+    if(estado_juego->game_over)
+    {
         *estado_app = ESTADO_GAMEOVER;
+        return;
     }
 
     dibujar_tablero(estado_juego, ventana);
@@ -210,15 +381,43 @@ void actualizar_jugando(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJu
     dibujar_ui(estado_juego, ventana);
 }
 
-void actualizar_gameover(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana) {
-    dibujar_interfaz_game_over(estado_juego, ventana);
-
-    if (tecla == GBTK_ESPACIO || tecla == GBTK_ESCAPE) {
+//--Pausa
+void actualizar_pausado(EstadoAplicacion* estado_app, eGBT_Tecla tecla, ResolucionVentana* ventana)
+{
+    dibujar_pantalla_pausa(ventana);
+    if(tecla == GBTK_p || tecla == GBTK_ENTER)
+    {
+        *estado_app = ESTADO_JUGANDO;
+    }
+    else if(tecla == GBTK_ESCAPE)
+    {
         *estado_app = ESTADO_MENU;
     }
 }
 
-int reiniciar_entorno_grafico(ResolucionVentana* ventana) {
+//--Game Over
+
+void actualizar_gameover(EstadoAplicacion* estado_app, eGBT_Tecla tecla, EstadoJuego* estado_juego, ResolucionVentana* ventana, Jugador* jug)
+{
+    dibujar_interfaz_game_over(estado_juego, ventana, jug);
+
+    // Actualiza mejor pntaje en caso de corresponder
+    if (estado_juego->puntos > jug->mejor_puntaje)
+    {
+        jug->mejor_puntaje = estado_juego->puntos;
+        jug->es_vga  = ventana->es_vga;
+        jug->escala  = ventana->escala;
+        guardar_jugador(jug);
+    }
+
+    if (tecla == GBTK_ESPACIO || tecla == GBTK_ESCAPE || tecla == GBTK_ENTER)
+    {
+        *estado_app = ESTADO_MENU;
+    }
+}
+
+int reiniciar_entorno_grafico(ResolucionVentana* ventana)
+{
     gbt_destruir_ventana();
     gbt_cerrar();
     if (inicializar_graficos(ventana, ventana->escala, ventana->es_vga) == -1) return -1;
